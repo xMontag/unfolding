e849afd837296316d1a8d7aca0c163875681f93a
WorkMontag <GuyVMontag@gmail.com>
2017-06-25 19:27:43 +0300

    first commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,242 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vitalii.Zotov
+ * Date: 23.06.2017
+ * Time: 20:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace unfolding
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox diameterTextBox;
+		private System.Windows.Forms.TextBox thicknessTextBox;
+		private System.Windows.Forms.ComboBox chamferTopComboBox;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ComboBox chamferBotComboBox;
+		private System.Windows.Forms.Label Angle;
+		private System.Windows.Forms.TextBox angleTrimTextBox;
+		private System.Windows.Forms.TextBox angleRotateTextBox;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox nPartTextBox;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox angleSectorTextBox;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button buttonDo;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.diameterTextBox = new System.Windows.Forms.TextBox();
+			this.thicknessTextBox = new System.Windows.Forms.TextBox();
+			this.chamferTopComboBox = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.chamferBotComboBox = new System.Windows.Forms.ComboBox();
+			this.Angle = new System.Windows.Forms.Label();
+			this.angleTrimTextBox = new System.Windows.Forms.TextBox();
+			this.angleRotateTextBox = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.nPartTextBox = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.angleSectorTextBox = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.buttonDo = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(13, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(59, 17);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Diameter";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(13, 43);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(59, 17);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Thickness";
+			// 
+			// diameterTextBox
+			// 
+			this.diameterTextBox.Location = new System.Drawing.Point(100, 13);
+			this.diameterTextBox.Name = "diameterTextBox";
+			this.diameterTextBox.Size = new System.Drawing.Size(100, 20);
+			this.diameterTextBox.TabIndex = 2;
+			// 
+			// thicknessTextBox
+			// 
+			this.thicknessTextBox.Location = new System.Drawing.Point(100, 40);
+			this.thicknessTextBox.Name = "thicknessTextBox";
+			this.thicknessTextBox.Size = new System.Drawing.Size(100, 20);
+			this.thicknessTextBox.TabIndex = 3;
+			// 
+			// chamferTopComboBox
+			// 
+			this.chamferTopComboBox.FormattingEnabled = true;
+			this.chamferTopComboBox.Items.AddRange(new object[] {
+			"External",
+			"Internal"});
+			this.chamferTopComboBox.Location = new System.Drawing.Point(100, 67);
+			this.chamferTopComboBox.Name = "chamferTopComboBox";
+			this.chamferTopComboBox.Size = new System.Drawing.Size(100, 21);
+			this.chamferTopComboBox.TabIndex = 4;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(13, 70);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(68, 18);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Chamfer top";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(13, 97);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(68, 18);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Chamfer bot";
+			// 
+			// chamferBotComboBox
+			// 
+			this.chamferBotComboBox.FormattingEnabled = true;
+			this.chamferBotComboBox.Items.AddRange(new object[] {
+			"External",
+			"Internal"});
+			this.chamferBotComboBox.Location = new System.Drawing.Point(100, 94);
+			this.chamferBotComboBox.Name = "chamferBotComboBox";
+			this.chamferBotComboBox.Size = new System.Drawing.Size(100, 21);
+			this.chamferBotComboBox.TabIndex = 8;
+			// 
+			// Angle
+			// 
+			this.Angle.Location = new System.Drawing.Point(13, 124);
+			this.Angle.Name = "Angle";
+			this.Angle.Size = new System.Drawing.Size(68, 17);
+			this.Angle.TabIndex = 9;
+			this.Angle.Text = "Angle trim";
+			// 
+			// angleTrimTextBox
+			// 
+			this.angleTrimTextBox.Location = new System.Drawing.Point(100, 121);
+			this.angleTrimTextBox.Name = "angleTrimTextBox";
+			this.angleTrimTextBox.Size = new System.Drawing.Size(100, 20);
+			this.angleTrimTextBox.TabIndex = 10;
+			// 
+			// angleRotateTextBox
+			// 
+			this.angleRotateTextBox.Location = new System.Drawing.Point(100, 147);
+			this.angleRotateTextBox.Name = "angleRotateTextBox";
+			this.angleRotateTextBox.Size = new System.Drawing.Size(100, 20);
+			this.angleRotateTextBox.TabIndex = 12;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(13, 150);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(68, 17);
+			this.label5.TabIndex = 11;
+			this.label5.Text = "Angle rotate";
+			// 
+			// nPartTextBox
+			// 
+			this.nPartTextBox.Location = new System.Drawing.Point(100, 199);
+			this.nPartTextBox.Name = "nPartTextBox";
+			this.nPartTextBox.Size = new System.Drawing.Size(100, 20);
+			this.nPartTextBox.TabIndex = 14;
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(13, 202);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(68, 17);
+			this.label6.TabIndex = 13;
+			this.label6.Text = "N part ";
+			// 
+			// angleSectorTextBox
+			// 
+			this.angleSectorTextBox.Location = new System.Drawing.Point(100, 173);
+			this.angleSectorTextBox.Name = "angleSectorTextBox";
+			this.angleSectorTextBox.Size = new System.Drawing.Size(100, 20);
+			this.angleSectorTextBox.TabIndex = 16;
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(13, 176);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(68, 17);
+			this.label7.TabIndex = 15;
+			this.label7.Text = "Angle sector";
+			// 
+			// buttonDo
+			// 
+			this.buttonDo.Location = new System.Drawing.Point(125, 237);
+			this.buttonDo.Name = "buttonDo";
+			this.buttonDo.Size = new System.Drawing.Size(75, 23);
+			this.buttonDo.TabIndex = 17;
+			this.buttonDo.Text = "Do";
+			this.buttonDo.UseVisualStyleBackColor = true;
+			this.buttonDo.Click += new System.EventHandler(this.ButtonDoClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(215, 273);
+			this.Controls.Add(this.buttonDo);
+			this.Controls.Add(this.angleSectorTextBox);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.nPartTextBox);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.angleRotateTextBox);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.angleTrimTextBox);
+			this.Controls.Add(this.Angle);
+			this.Controls.Add(this.chamferBotComboBox);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.chamferTopComboBox);
+			this.Controls.Add(this.thicknessTextBox);
+			this.Controls.Add(this.diameterTextBox);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "unfolding";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+using TS = Tekla.Structures;
+using TSD = Tekla.Structures.Drawing;
+using TSG = Tekla.Structures.Geometry3d;
+
+namespace unfolding
+	
+{
+	public partial class MainForm : Form
+	{
+		
+		
+		public MainForm()
+		{
+			InitializeComponent();
+			
+			diameterTextBox.Text = "2720";
+			thicknessTextBox.Text="10";
+			chamferTopComboBox.SelectedItem = "Internal";
+			chamferBotComboBox.SelectedItem = "Internal";
+			angleTrimTextBox.Text = "22,5";
+			angleRotateTextBox.Text = "90";
+			angleSectorTextBox.Text = "359,958";
+			nPartTextBox.Text = "96";
+		
+		}
+		
+		
+		
+		void ButtonDoClick(object sender, EventArgs e)
+		{
+			TSD.DrawingHandler drawingHandler = new TSD.DrawingHandler();
+			
+			TSD.UI.Picker picker = drawingHandler.GetPicker();
+			
+			TSG.Point point1 = null;
+			TSG.Point point2 = null;
+			TSG.Point point3 = null;
+			TSD.ViewBase view = null;
+			
+			Shell shell = null;
+			
+			try
+			{
+				shell = new Shell(Convert.ToDouble(diameterTextBox.Text), Convert.ToDouble(thicknessTextBox.Text), chamferTopComboBox.SelectedItem.ToString(), chamferBotComboBox.SelectedItem.ToString(), Convert.ToDouble(angleTrimTextBox.Text), Convert.ToDouble(angleRotateTextBox.Text), Convert.ToDouble(angleSectorTextBox.Text), Convert.ToInt32(nPartTextBox.Text));
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+			
+			if (shell != null)
+			{
+				try
+				{
+					picker.PickThreePoints("1st poit", "2nd point", "3rd point", out point1, out point2, out point3, out view);
+				}
+				catch(Exception ex)
+				{
+					MessageBox.Show(ex.ToString());
+				}
+				
+				
+				double angleP2 = GetAngle(new TSG.Point((point2.X - point1.X), (point2.Y - point1.Y)));
+				double angleP3 = GetAngle(new TSG.Point(((point3.X - point1.X)*Math.Cos(-angleP2) - (point3.Y - point1.Y)*Math.Sin(-angleP2)), ((point3.X - point1.X)*Math.Sin(-angleP2) + (point3.Y - point1.Y)*Math.Cos(-angleP2))));
+				
+				int kof = ( (angleP3 >= 0) && (angleP3 < Math.PI) ) ? -1 : 1;
+				
+				double lArc = (shell.diameter/2 - shell.thickness/2) * shell.angleSector;
+				
+				for (int i = 0; i < shell.arrLine.Length; i++)
+				{
+					TSG.Point p1 = new TSG.Point( (point1.X + (i*lArc/shell.nPart)*Math.Cos(angleP2)) , (point1.Y + (i*lArc/shell.nPart)*Math.Sin(angleP2)) );
+					TSG.Point p2 = new TSG.Point( (point1.X + i*lArc/shell.nPart*Math.Cos(angleP2) + kof*shell.arrLine[i]*Math.Sin(angleP2) ) , (point1.Y + i*lArc/shell.nPart*Math.Sin(angleP2) - kof*shell.arrLine[i]*Math.Cos(angleP2)));;
+					
+					TSD.Text.TextAttributes txtAtributes = new TSD.Text.TextAttributes();
+					txtAtributes.Angle = angleP2*180/Math.PI + 90;
+					
+					string dimText = (Math.Round(shell.arrLine[i]).ToString());
+					TSD.Text dim = new TSD.Text(view, p1, dimText, txtAtributes);
+					dim.Insert();
+					
+					TSD.Line line = new TSD.Line(view, p1, p2);
+					line.Insert();
+
+					
+					
+				}
+				
+				
+				
+				TSD.Line line2 = new TSD.Line(view, point1, new TSG.Point((point1.X + lArc*Math.Cos(angleP2)) , (point1.Y + lArc*Math.Sin(angleP2))));
+				line2.Insert();
+				
+				
+			}
+		}
+		
+		double GetAngle(TSG.Point point)
+		{
+			double ang = 0;
+			
+			if ( point.Y == 0 && point.X > 0 ) return 0;
+			if ( point.Y == 0 && point.X < 0 ) return Math.PI/2;
+			if ( point.X >= 0 && point.Y > 0 ) return Math.Atan( point.Y / point.X );
+			if ( point.X >= 0 && point.Y < 0 ) return Math.PI*2 - Math.Abs(Math.Atan(point.Y/point.X));
+			if ( point.X < 0 && point.Y < 0 ) return Math.PI + Math.Abs(Math.Atan(point.Y/point.X));
+			if ( point.X < 0 && point.Y < 0 ) return Math.PI - Math.Abs(Math.Atan(point.Y/point.X));
+
+			return ang;
+		}
+	}
+	
+	
+	
+	public class Shell
+	{
+		public double diameter;
+		public double thickness;
+		public string chamferTop;
+		public string chamferBot;
+		public double angleTrim;
+		public double angleRotate;
+		public double angleSector;
+		public int nPart;
+		public List<double> lines;
+		public double[] arrLine;
+		
+		public Shell(double diameter, double thickness, string chamferTop, string chamferBot, double angleTrim, double angleRotate, double angleSector, int nPart)
+		{
+			this.diameter = diameter;
+			this.thickness = thickness;
+			this.chamferTop = chamferTop;
+			this.chamferBot = chamferBot;
+			this.angleTrim = angleTrim * Math.PI/180;
+			this.angleRotate = angleRotate * Math.PI/180;
+			this.angleSector = angleSector * Math.PI/180;
+			this.nPart = nPart;
+			double a;
+			double a_l;
+			double l;
+			lines = new List<double>();
+			arrLine = new double[nPart+1];
+			
+			for (int i = 0; i < nPart+1; i++) 
+			{
+				a = this.angleRotate + i * (this.angleSector / nPart);
+				a_l = Math.Atan( Math.Cos(a) * Math.Tan(this.angleTrim) );
+				l = ( (this.diameter/2 - this.thickness/2) * Math.Tan(a_l) );
+				if (a > 2*Math.PI)
+				{
+					a -= 2*Math.PI;
+				}
+				
+				if ( (a >= 1.5*Math.PI && a <= 2*Math.PI) || (a >= 0 && a <= 0.5*Math.PI) )
+				{
+					if (chamferTop.Equals("Internal"))
+					{
+						l = l - (this.thickness/2)*Math.Abs(Math.Tan(a_l));
+					}
+					else
+					{
+						l = l + (this.thickness/2)*Math.Abs(Math.Tan(a_l));
+					}
+				}
+				else
+				{
+					if (chamferBot.Equals("Internal"))
+					{
+						l = l + (this.thickness/2)*Math.Abs(Math.Tan(a_l));
+					}
+					else
+					{
+						l = l - (this.thickness/2)*Math.Abs(Math.Tan(a_l));
+					}
+				}
+				
+				this.arrLine[i] = l;
+				this.lines.Add(l);
+			}
+			
+			double lMin =  this.arrLine[0];
+			
+			foreach (double el in this.arrLine)
+			{
+				if (el < lMin)
+				{
+					lMin = el;
+				}
+			}
+			
+			for (int i = 0; i < this.arrLine.Length; i++)
+			{
+				this.arrLine[i] = this.arrLine[i] - lMin;
+			}
+		}
+	}
+}
